a5c232b499bc581a192315c14bf72efec53d48cc
sinta <sinta@icehousecorp.com>
2016-11-02 11:22:14 +0700

    Prepare for data conversion

---
+++ ChartsSampleProject/ChartsSampleProject/DataConverters.swift
@@ -0,0 +1,31 @@
+//
+//  DataConverters.swift
+//  ChartsSampleProject
+//
+//  Created by Sinta Amanda Tangkudung on 11/2/16.
+//  Copyright © 2016 Sinta Amanda Tangkudung. All rights reserved.
+//
+
+import UIKit
+import Charts
+
+class DataConverter {
+  
+  func createLineChartData(lineChartDataEntry: [LineChartDataModel]) -> LineChartData {
+    // Implement here
+    
+    return LineChartData()
+  }
+  
+  func createPieChartData(pieChartDataEntry: [PieChartDataModel]) -> PieChartData {
+    // Implement here
+    
+    return PieChartData()
+  }
+  
+  func createBarChartData(barChartData: [BarChartDataModel]) -> BarChartData {
+    // Implement here
+    
+    return BarChartData()
+  }
+}
